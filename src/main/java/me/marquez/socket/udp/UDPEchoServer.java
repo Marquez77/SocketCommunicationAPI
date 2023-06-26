746eab2e7bb3d3092bd8f97d908ae4045679e91e
Marquez77 <62211625+Marquez77@users.noreply.github.com>
2023-06-26 16:15:32 +0900

    Solve the problem that concurrency when sending

--- src/main/java/me/marquez/socket/udp/UDPEchoServer.java
+++ src/main/java/me/marquez/socket/udp/UDPEchoServer.java
@@ -14,10 +14,12 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class UDPEchoServer extends Thread{
 
     private static final long TIMEOUT = 30; //UDP 응답 최대 대기 시간 (초)
+    private static final AtomicInteger identifier = new AtomicInteger(0);
 
     private final int serverPort;
     private final Logger logger;
@@ -39,8 +41,8 @@ public class UDPEchoServer extends Thread{
         handlers.add(handler);
     }
 
-    //timestamp, response future
-    private final Map<Long, CompletableFuture<UDPEchoResponse>> echoMap = new ConcurrentHashMap<>();
+    //identifier, response future
+    private final Map<Integer, CompletableFuture<UDPEchoResponse>> echoMap = new ConcurrentHashMap<>();
 
     @Override
     public void run() {
@@ -53,15 +55,15 @@ public class UDPEchoServer extends Thread{
 //                logger.info("Receiving: {}", receiveData.getData());
                 String str = new String(receiveData.getData(), receiveData.getOffset(), receiveData.getLength(), StandardCharsets.UTF_8);
                 String[] split = str.split(";", 2);
-                final long timestamp = Long.parseLong(split[0]);
+                final int id = Integer.parseInt(split[0]);
 
                 InetAddress address = receiveData.getAddress();
                 int port = receiveData.getPort();
 //                logger.info("[{}:{}] timestamp: {}", address.getHostAddress(), port, timestamp);
 
-                if(echoMap.containsKey(timestamp)) { //이곳에서 보낸 데이터일 경우 Future Complete
+                if(echoMap.containsKey(id)) { //이곳에서 보낸 데이터일 경우 Future Complete
                     logger.info("[CURRENT->{}:{}->CURRENT] Received echo data: {}", address.getHostAddress(), port, str);
-                    echoMap.computeIfPresent(timestamp,(k, v) -> {
+                    echoMap.computeIfPresent(id,(k, v) -> {
                         v.complete(new UDPEchoResponse(split[1]));
                         return null;
                     });
@@ -75,7 +77,7 @@ public class UDPEchoServer extends Thread{
                         handlers.forEach(udpMessageHandler -> udpMessageHandler.onReceive(new InetSocketAddress(address, port), send.clone(), response));
                         logger.info("[CURRENT->{}:{}] Response data: {}", address.getHostAddress(), port, response);
 
-                        byte[] responseBuffer = (timestamp + ";" + response.toString()).getBytes(StandardCharsets.UTF_8);
+                        byte[] responseBuffer = (id + ";" + response.toString()).getBytes(StandardCharsets.UTF_8);
                         DatagramPacket sendData = new DatagramPacket(responseBuffer, responseBuffer.length, address, port);
                         try {
                             serverSocket.send(sendData);
@@ -101,15 +103,15 @@ public class UDPEchoServer extends Thread{
     }
 
     public CompletableFuture<UDPEchoResponse> sendDataAndReceive(SocketAddress host, final UDPEchoSend data) {
-        long timestamp = System.currentTimeMillis();
+        int id = identifier.getAndIncrement();
+        while(echoMap.containsKey(id)) id = identifier.getAndIncrement();
         CompletableFuture<UDPEchoResponse> future = new CompletableFuture<>();
-        while(echoMap.containsKey(timestamp)) timestamp += 1L;
-        echoMap.put(timestamp, future);
+        echoMap.put(id, future);
         InetSocketAddress address = (InetSocketAddress)host;
         logger.info("[CURRENT->{}:{}] Sent data: {}", address.getHostString(), address.getPort(), data);
-        final long id = timestamp;
+        final int finalId = id;
         Executors.newCachedThreadPool().submit(() -> {
-            byte[] buffer = (id + ";" + data).getBytes(StandardCharsets.UTF_8);
+            byte[] buffer = (finalId + ";" + data).getBytes(StandardCharsets.UTF_8);
             DatagramPacket sendData = new DatagramPacket(buffer, buffer.length, host);
             try {
                 serverSocket.send(sendData);
@@ -127,7 +129,7 @@ public class UDPEchoServer extends Thread{
 //            logger.info("Compare data:\t \n\t\tSent:\t{}1\n\t\tReceive:\t{}1\nequals: {}", data, result, data.equals(result));
 //            if(throwable == null && data.equals(result)) future.complete(null); //보낸 데이터와 받은 데이터가 일치 할 때
             if(throwable == null) future.complete(null);
-            future.completeExceptionally(new NoEchoDataException());
+            else future.completeExceptionally(new NoEchoDataException());
         });
         return future;
     }
