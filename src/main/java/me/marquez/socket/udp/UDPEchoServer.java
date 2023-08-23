6ecd5804b5a1625b566d010850655b263aed0a4e
Marquez77(Office) <hyunjun002@gmail.com>
2023-08-23 20:23:16 +0900

    Update handling big data communication

--- src/main/java/me/marquez/socket/udp/UDPEchoServer.java
+++ src/main/java/me/marquez/socket/udp/UDPEchoServer.java
@@ -1,17 +1,16 @@
 package me.marquez.socket.udp;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import me.marquez.socket.udp.entity.UDPEchoData;
 import me.marquez.socket.udp.entity.UDPEchoResponse;
 import me.marquez.socket.udp.entity.UDPEchoSend;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 
 import java.io.IOException;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
@@ -62,6 +61,26 @@ public class UDPEchoServer extends Thread{
 
     //identifier, response future
     private final Map<Long, CompletableFuture<UDPEchoResponse>> echoMap = new ConcurrentHashMap<>();
+    private final Map<Long, BigData> bigDataMap = new ConcurrentHashMap<>();
+
+    private static class BigData {
+        int targetLength;
+        int currentLength;
+        String[] data;
+
+        public BigData(int length) {
+            this.targetLength = length;
+            this.currentLength = 0;
+            int size = (int)Math.ceil((double)length/65000D);
+            this.data = new String[size];
+        }
+
+        public boolean add(int i, String data) {
+            this.data[i] = data;
+            currentLength += data.length();
+            return currentLength == targetLength;
+        }
+    }
 
     @Override
     public void run() {
@@ -71,41 +90,7 @@ public class UDPEchoServer extends Thread{
                 byte[] buffer = new byte[serverSocket.getReceiveBufferSize()];
                 final DatagramPacket receiveData = new DatagramPacket(buffer, buffer.length);
                 serverSocket.receive(receiveData);
-//                logger.info("Receiving: {}", receiveData.getData());
-                String str = new String(receiveData.getData(), receiveData.getOffset(), receiveData.getLength(), StandardCharsets.UTF_8);
-                String[] split = str.split(";", 2);
-                final long id = Long.parseLong(split[0]);
-
-                InetAddress address = receiveData.getAddress();
-                int port = receiveData.getPort();
-//                logger.info("[{}:{}] timestamp: {}", address.getHostAddress(), port, timestamp);
-
-                if(echoMap.containsKey(id)) { //이곳에서 보낸 데이터일 경우 Future Complete
-                    info("[CURRENT->{}:{}->CURRENT] Received echo data: {}", address.getHostAddress(), port, str);
-                    echoMap.computeIfPresent(id,(k, v) -> {
-                        v.complete(UDPEchoResponse.of(split[1]));
-                        return null;
-                    });
-
-                }else { //다른 곳에서 받은 데이터일 경우 데이터 되돌려주기
-                    info("[{}:{}->CURRENT] Received data: {}", address.getHostAddress(), port, str);
-
-                    Executors.newCachedThreadPool().submit(() -> { //onReceive 메소드 대기 중에도 통신 가능하도록 비동기 실행
-                        UDPEchoSend send = UDPEchoSend.of(split[1]); //수신 데이터 만들기
-                        UDPEchoResponse response = new UDPEchoResponse(); //반환 데이터 만들기
-                        handlers.forEach(udpMessageHandler -> udpMessageHandler.onReceive(new InetSocketAddress(address, port), send.clone(), response));
-                        info("[CURRENT->{}:{}] Response data: {}", address.getHostAddress(), port, response);
-
-                        byte[] responseBuffer = (id + ";" + response.toString()).getBytes(StandardCharsets.UTF_8);
-                        DatagramPacket sendData = new DatagramPacket(responseBuffer, responseBuffer.length, address, port);
-                        try {
-                            serverSocket.send(sendData);
-//                            logger.info("Sending: {}", sendData.getData());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    });
-                }
+                Executors.newCachedThreadPool().submit(() -> onReceive(receiveData));
             }catch(IOException e) {
                 if(e.getMessage().contains("socket closed")) logger.info("socket closed");
                 else logger.warn("IOException: {}", e.getMessage());
@@ -121,6 +106,91 @@ public class UDPEchoServer extends Thread{
         }
     }
 
+    private void onReceive(DatagramPacket receiveData) {
+
+//                logger.info("Receiving: {}", receiveData.getData());
+        String str = new String(receiveData.getData(), receiveData.getOffset(), receiveData.getLength(), StandardCharsets.UTF_8);
+
+        InetAddress address = receiveData.getAddress();
+        int port = receiveData.getPort();
+
+        //1 + headerLength + 1 + i + 1 + 14 + 1 + data <= 65507
+        if(str.startsWith("!")) {
+            String[] split = str.substring(1).split(";", 4);
+            int length = Integer.parseInt(split[0]);
+            int i = Integer.parseInt(split[1]);
+            long id = Long.parseLong(split[2]);
+            String data = split[3];
+            info("[{}:{}->CURRENT] Received bit data: {}", address.getHostAddress(), port, str);
+            BigData bigData = bigDataMap.computeIfAbsent(id, k-> new BigData(length));
+            if (!bigData.add(i, data)) return;
+            bigDataMap.remove(id);
+            str = id + ";" + String.join("", bigData.data);
+        }
+
+        String[] split = str.split(";", 2);
+        final long id = Long.parseLong(split[0]);
+//                logger.info("[{}:{}] timestamp: {}", address.getHostAddress(), port, timestamp);
+
+        if(echoMap.containsKey(id)) { //이곳에서 보낸 데이터일 경우 Future Complete
+            info("[CURRENT->{}:{}->CURRENT] Received echo data: {}", address.getHostAddress(), port, str);
+            echoMap.computeIfPresent(id,(k, v) -> {
+                v.complete(UDPEchoResponse.of(split[1]));
+                return null;
+            });
+
+        }else { //다른 곳에서 받은 데이터일 경우 데이터 되돌려주기
+            info("[{}:{}->CURRENT] Received data: {}", address.getHostAddress(), port, str);
+
+            Executors.newCachedThreadPool().submit(() -> { //onReceive 메소드 대기 중에도 통신 가능하도록 비동기 실행
+                UDPEchoSend send = UDPEchoSend.of(split[1]); //수신 데이터 만들기
+                UDPEchoResponse response = new UDPEchoResponse(); //반환 데이터 만들기
+                InetSocketAddress host = new InetSocketAddress(address, port);
+                handlers.forEach(udpMessageHandler -> udpMessageHandler.onReceive(host, send.clone(), response));
+                info("[CURRENT->{}:{}] Response data: {}", address.getHostAddress(), port, response);
+                sendData(response, id, host, null);
+            });
+        }
+    }
+
+    private static final int MAX_PACKET_SIZE = 65507;
+
+    private void sendData(UDPEchoData data, long id, SocketAddress host, CompletableFuture<?> future) {
+        String serializedData = data.toString();
+        int length = serializedData.length();
+        // 데이터가 최대 패킷 크기보다 클 경우 분할해서 전송
+        if(length + 14 + 1 > MAX_PACKET_SIZE) { //id + ; = 14 + 1
+            sendBigData(length, id, serializedData, host, future);
+        }else {
+            sendData("", id, serializedData, host, future);
+        }
+    }
+
+    private void sendData(String header, long finalId, String data, SocketAddress host, @Nullable CompletableFuture<?> future) {
+        byte[] buffer = (header + finalId + ";" + data).getBytes(StandardCharsets.UTF_8);
+        DatagramPacket sendData = new DatagramPacket(buffer, buffer.length, host);
+        try {
+            serverSocket.send(sendData);
+        } catch (Exception e) {
+            e.printStackTrace();
+            if(future != null) future.completeExceptionally(e);
+            else e.printStackTrace();
+        }
+    }
+    private void sendBigData(int length, long finalId, String serializedData, SocketAddress host, CompletableFuture<?> future) {
+        //1 + headerLength + 1 + i + 1 + 14 + 1 + data <= 65507
+        int headerLength = String.valueOf(length).length();
+        int defaultUnit = MAX_PACKET_SIZE-18-headerLength;
+        for(int index = 0, i = 0;; i++) {
+            int unit = defaultUnit - String.valueOf(i).length();
+            String header = "!" + length + ";" + i + ";";
+            int endIndex = Math.min(index + unit, length);
+            String bitData = serializedData.substring(index, endIndex);
+            sendData(header, finalId, bitData, host, future);
+            if (endIndex == length) break;
+            index += unit;
+        }
+    }
     public CompletableFuture<UDPEchoResponse> sendDataAndReceive(SocketAddress host, final UDPEchoSend data) {
         long id = makeId();
         while(echoMap.containsKey(id)) id = makeId();
@@ -129,15 +199,7 @@ public class UDPEchoServer extends Thread{
         InetSocketAddress address = (InetSocketAddress)host;
         info("[CURRENT->{}:{}] Sent data: {}", address.getHostString(), address.getPort(), data);
         final long finalId = id;
-        Executors.newCachedThreadPool().submit(() -> {
-            byte[] buffer = (finalId + ";" + data).getBytes(StandardCharsets.UTF_8);
-            DatagramPacket sendData = new DatagramPacket(buffer, buffer.length, host);
-            try {
-                serverSocket.send(sendData);
-            } catch (IOException e) {
-                future.completeExceptionally(e);
-            }
-        });
+        Executors.newCachedThreadPool().submit(() -> sendData(data, finalId, host, future));
         return future;
     }
 
---
+++ src/test/java/me/marquea/socket/udp/BigDataTest.java
@@ -0,0 +1,42 @@
+package me.marquea.socket.udp;
+
+import me.marquez.socket.udp.UDPEchoServer;
+import me.marquez.socket.udp.entity.UDPEchoSend;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class BigDataTest {
+    public static void main(String[] args) throws IOException {
+        UDPEchoServer server = new UDPEchoServer(8280, LoggerFactory.getLogger("server1"));
+//        server.setDebug(true);
+        server.start();
+
+        UDPEchoServer server2 = new UDPEchoServer(8281, LoggerFactory.getLogger("server2"));
+//        server2.setDebug(true);
+        server2.registerHandler((client, send, response) -> {
+            System.out.println(send.toString().length());
+        });
+        server2.start();
+
+        UDPEchoSend send = new UDPEchoSend();
+        for(int i = 0; i < 10000; i++) {
+            send.append("aaaaaaaaaaaaaaaaaaaa");
+        }
+        System.out.println(send);
+        System.out.println(send.toString().length());
+        server.sendDataAndReceive(new InetSocketAddress("localhost", 8281), send)
+                .whenComplete((udpEchoResponse, throwable) -> {
+                    if(throwable != null) {
+                        throwable.printStackTrace();
+                        return;
+                    }
+                    System.out.println(udpEchoResponse);
+                    System.exit(0);
+        }).exceptionally(throwable -> {
+            throwable.printStackTrace();
+            return null;
+                });
+    }
+}
