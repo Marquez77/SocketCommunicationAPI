9900ab44ce12354f5a5337802663c3db070e8fe3
Marquez77 <62211625+Marquez77@users.noreply.github.com>
2023-10-18 08:04:50 +0900

    Add LongDataParsingTest

---
+++ src/test/java/LongDataParsingTest.java
@@ -0,0 +1,24 @@
+import me.marquez.socket.udp.UDPEchoServer;
+import me.marquez.socket.udp.entity.UDPEchoSend;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class LongDataParsingTest {
+    public static void main(String[] args) throws IOException {
+        UDPEchoServer server = new UDPEchoServer(8080, LoggerFactory.getLogger("server1"));
+        server.registerHandler((client, send, response) -> {
+            System.out.println("server1: " + send.nextLong());
+        });
+        server.start();
+
+        UDPEchoServer server2 = new UDPEchoServer(8081, LoggerFactory.getLogger("server2"));
+        server2.registerHandler((client, send, response) -> {
+            System.out.println("server2: " + send.nextLong());
+        });
+        server2.start();
+
+        server.sendDataAndReceive(new InetSocketAddress("localhost", 8081), new UDPEchoSend(System.currentTimeMillis()));
+    }
+}
