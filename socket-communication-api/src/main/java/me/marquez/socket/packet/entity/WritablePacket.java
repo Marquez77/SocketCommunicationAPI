cf0575161e18f59c2c99c0e155c3ebd8c00e435e
Marquez77 <62211625+Marquez77@users.noreply.github.com>
2023-12-13 15:46:21 +0900

    Add api methods for WritablePacket

--- socket-communication-api/src/main/java/me/marquez/socket/packet/entity/WritablePacket.java
+++ socket-communication-api/src/main/java/me/marquez/socket/packet/entity/WritablePacket.java
@@ -23,4 +23,47 @@ public interface WritablePacket {
     WritablePacket append(BigInteger bi);
     WritablePacket append(BigDecimal bd);
     WritablePacket append(UUID uuid);
+
+    default WritablePacket appendByte(Object b) {
+        return append((byte)b);
+    }
+    default WritablePacket appendBytes(Object bytes) {
+        return append((byte[])bytes);
+    }
+    default WritablePacket appendChar(Object c) {
+        return append((char)c);
+    }
+    default WritablePacket appendShort(Object s) {
+        return append((short)s);
+    }
+    default WritablePacket appendInt(Object i) {
+        if(i instanceof Number n)
+            return append(n.intValue());
+        return append((int)i);
+    }
+    default WritablePacket appendLong(Object l) {
+        if(l instanceof Number n)
+            return append(n.longValue());
+        return append((long)l);
+    }
+    default WritablePacket appendFloat(Object f) {
+        if(f instanceof Number n)
+            return append(n.floatValue());
+        return append((float)f);
+    }
+    default WritablePacket appendDouble(Object d) {
+        if(d instanceof Number n)
+            return append(n.doubleValue());
+        return append((double)d);
+    }
+    default WritablePacket appendString(Object s) {
+        if(s instanceof String str)
+            return append(str);
+        return append(s.toString());
+    }
+    default WritablePacket appendUUID(Object uuid) {
+        if(uuid instanceof UUID uuidInstance)
+            return append(uuidInstance);
+        return append(UUID.fromString(uuid.toString()));
+    }
 }
